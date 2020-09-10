61c13f89c3ffadaada0dda5be0b20969d5b9f008
gigurururu <gigurururu@gmail.com>
2020-09-10 23:37:17 +0900

    POSTの中身をそのまま返すようお試し

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.listen(3000, ()=>{
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({extended:true}));
+
+app.post('/api',(req,res,next)=>{
+  console.log(req.body);
+  res.send(req.body);
+});
+
 app.use((req, res)=>{
   res.sendStatus(404);
 });
