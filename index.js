45fc2e958592e97407e7a93f24045cc73f77f759
gigurururu <gigurururu@gmail.com>
2020-09-14 00:39:50 +0900

    form送信後は元のページにリダイレクト

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ app.listen(3000, ()=>{
   console.log('Example app listening on port 3000!');
 });
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({extended:true}));
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.post('/api',(req,res,next)=>{
   console.log(req.body);
-  res.send(req.body);
+  res.redirect('/static');
 });
 
 app.use((req, res)=>{
