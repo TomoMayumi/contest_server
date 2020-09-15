4d2d551c7e5b55c0a168a6cda22bb44bc9e7d9f8
gigurururu <gigurururu@gmail.com>
2020-09-16 00:48:45 +0900

    受け取った文字列をRuby実行してコンソール出力

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require('path');
+const {execSync} = require('child_process');
 
 app.listen(3000, ()=>{
   console.log('Example app listening on port 3000!');
@@ -11,7 +13,27 @@ app.use(express.urlencoded({extended:true}));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.post('/api',(req,res,next)=>{
-  console.log(req.body);
+  const code = req.body.code;
+  const dir = './tmp';
+  const file = path.join(dir,'tmp.rb');
+
+  if(!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  fs.writeFileSync(file,code);
+
+  try{
+    execSync(`ruby -c ${file}`);
+    try{
+      const result = execSync(`ruby ${file}`);
+      console.log({result:result.toString()});
+    }catch(err){
+      console.log({stderr:err.stderr.toString(),stdout:err.stdout.toString()});
+    }
+  }catch(err){
+    console.log({stderr:err.stderr.toString(),stdout:err.stdout.toString()});
+  }
+  fs.unlinkSync(file);
   res.redirect('/static');
 });
 
